023ee228ce77b28e49af9446b2973e4f6763417b
Unknownkratos <60856458+Unknownkratos@users.noreply.github.com>
2023-08-07 19:05:01 +0100

    commit

--- src/pages/left-Menu/Settings.svelte
+++ src/pages/left-Menu/Settings.svelte
@@ -1,58 +1,112 @@
 <script>
-  import { Page, Navbar, Block, BlockTitle, List, ListItem, Label, Input, Button } from 'framework7-svelte';
+  import { Page, Navbar, Block, BlockTitle, ListItem, Icon, List, Toggle, f7 } from 'framework7-svelte';
 
-  // Sample station data
-  let stations = [
-    { name: 'Station A', time: '08:00' },
-    { name: 'Station B', time: '09:15' },
-    { name: 'Station C', time: '10:30' },
-    // Add more stations here
-  ];
+  let globalTheme = 'light';
+  let number = "95 123 456";
+  let lang = "english";
+  let dark_mode = false;
 
-  let selectedStationA;
-  let selectedStationB;
-  let selectedTime;
-  let calculatedTime;
+  function setScheme(newTheme) {
+    console.log(newTheme);
+    f7.setDarkMode(newTheme === 'dark');
+    globalTheme = newTheme;
+  }
 
-  function calculateTimeDifference() {
-    // ... Same calculation code as before ...
+  function handleChange(e) {
+    lang = e.target.value;
+  }
 
-    // Set the calculated time as a reactive variable
-    calculatedTime = `${hours}:${minutes}`;
+  function handleToggle(event) {
+    dark_mode = event.target.checked;
+    setScheme(dark_mode ? 'dark' : 'light');
   }
 </script>
 
 <Page>
-  <Navbar title="Train Timeline" backLink="Back" />
-  <BlockTitle>About My App</BlockTitle>
+  <Navbar title="Settings" backLink="Back" />
+  <div class="inline-container-header">
+    <Icon f7="gear_alt_fill" style="margin-top: 15px;"></Icon>
+    <BlockTitle>Settings</BlockTitle>
+  </div>
   <Block>
-    <!-- List of stations to choose from -->
-    <List>
-      <ListItem>
-        <Label>From</Label>
-        <Input type="select" value={selectedStationA} on:change={(e) => (selectedStationA = e.target.value)}>
-          {#each stations as station}
-            <option value={station.name}>{station.name}</option>
-          {/each}
-        </Input>
-      </ListItem>
-      <ListItem>
-        <Label>To</Label>
-        <Input type="select" value={selectedStationB} on:change={(e) => (selectedStationB = e.target.value)}>
-          {#each stations as station}
-            <option value={station.name}>{station.name}</option>
-          {/each}
-        </Input>
-      </ListItem>
-      <ListItem>
-        <Label>Time at From Station</Label>
-        <Input type="time" value={selectedTime} on:change={(e) => (selectedTime = e.target.value)} />
-      </ListItem>
-    </List>
+    <table style="margin: 0 auto;">
+      <tr>
+        <td>
+          <a class="button button-round button-tonal" href>
+            <img class="icon" src='https://cdn-icons-png.flaticon.com/512/597/597177.png' alt="icon">
+            Number
+          </a>
+        </td>
+        <td>
+          <div class="input-container">
+            <input type="tel" class="phone-input" bind:value={number} />
+          </div>
+        </td>
+      </tr>
 
-    <Button onClick={calculateTimeDifference}>Calculate Time</Button>
-    {#if calculatedTime}
-      <div style="margin: 20px;">Time at Destination Station: {calculatedTime}</div>
-    {/if}
+      <tr>
+        <td>
+          <a class="button button-round button-tonal" href>
+            <img class="icon" src='https://cdn-icons-png.flaticon.com/512/2115/2115307.png' alt="icon">
+            Language
+          </a>
+        </td>
+
+        <td>
+          <List strongIos outlineIos dividersIos>
+            <ListItem title={lang} smartSelect>
+              <select name="Languages" bind:value={lang} style="text-align: center;">
+                <!--on:change={handleChange} -->
+                <option value="english">English</option>
+                <option value="french">French</option>
+                <option value="arabic">Arabic</option>
+              </select>
+            </ListItem>
+          </List>
+        </td>
+      </tr>
+
+      <tr>
+        <td>
+          <a class="button button-round button-tonal" href>
+            <img class="icon" src='https://cdn-icons-png.flaticon.com/512/8338/8338708.png' alt="icon">
+            Dark Mode
+          </a>
+        </td>
+
+        <td>
+          <div style="margin-left: 10px;">
+            <Toggle bind:checked={dark_mode} on:change={handleToggle} color="black" />
+          </div>
+        </td>
+      </tr>
+    </table>
   </Block>
 </Page>
+
+<style>
+  .icon {
+    width: 25px;
+  }
+
+  .input-container {
+    display: flex;
+    align-items: center;
+  }
+
+  .inline-container-header {
+    display: flex;
+    align-items: center;
+    gap: 1px;
+    margin-left: 10px;
+  }
+
+  .phone-input {
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    width: 200px;
+    outline: none;
+    text-align: center;
+  }
+</style>
