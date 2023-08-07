a544c35dde0d04ffedebc377f89678e219bf0e14
Unknownkratos <60856458+Unknownkratos@users.noreply.github.com>
2023-08-07 14:04:25 +0100

    commit

--- src/pages/left-Menu/Messages.svelte
+++ src/pages/left-Menu/Messages.svelte
@@ -1,79 +1,83 @@
 <Page>
-  <Navbar title="About" backLink="Back" />
+  <Navbar title="Messages" backLink="Back" />
   <div class="inline-container">
-    <Icon f7="tray_fill" style="margin-top: 15px;"></Icon> 
+    <Icon f7="tray_fill" style="margin-top: 15px;"></Icon>
     <BlockTitle>Messages</BlockTitle>
   </div>
-  
-  <!--person_fill  phone   person_crop_circle  gear_alt_fill   -->
+
+  <!-- person_fill  phone   person_crop_circle  gear_alt_fill   -->
 
   <div class="mailbox">
     {#each messages as message}
       <div class="message" key={message.id}>
         <img class="profile-pic" src={message.profilePic} alt="Profile Pic" />
-        <div>
-          <p style="font-size:larger;">{message.sender}</p>
-          <p style="font-size:small ;color:grey">{message.message}&nbsp;&nbsp;&nbsp;{message.send_time} </p>
+        <div class="message-content">
+          <p style="font-size: larger; font-weight: bold;">{message.sender}</p>
+          <p style="font-size: smaller; color: grey;">
+            {message.message}
+            <span style="margin-left: 10px;">{message.send_time}</span>
+          </p>
         </div>
       </div>
     {/each}
   </div>
- 
-  
-  
 </Page>
+
 <script>
-  import {BlockTitle,Navbar,Page,List,ListItem,} from 'framework7-svelte';
+  import { BlockTitle, Navbar, Page } from 'framework7-svelte';
   import Image from '../../../public/img/test.jpg';
   import { Icon } from 'framework7-svelte';
 
   let messages = [
     {
       id: 1,
-      sender: "John Doe",
-      message: "Hello there!",
+      sender: 'John Doe',
+      message: 'Hello there!',
       profilePic: Image,
-      send_time : "15:16"
+      send_time: '15:16',
     },
     {
       id: 2,
-      sender: "Jane Smith",
-      message: "Hi, how are you?",
+      sender: 'Jane Smith',
+      message: 'Hi, how are you?',
       profilePic: Image,
-      send_time : "09:55"
-    }
+      send_time: '09:55',
+    },
   ];
-
 </script>
 
 <style>
-    .inline-container {
+  .inline-container {
     display: flex;
     align-items: center;
     gap: 1px;
     margin-left: 10px;
-    
-    }
-
-
+  }
 
   .mailbox {
     display: flex;
     flex-direction: column;
     gap: 10px;
+    padding: 10px;
   }
 
   .message {
     display: flex;
     gap: 10px;
     align-items: center;
+    padding: 10px;
+    background-color: #f5f5f5;
+    border-radius: 10px;
   }
 
   .profile-pic {
     width: 75px;
     height: 75px;
     border-radius: 50%;
-    border: 10px;
-    margin: 10px;
+    margin-right: 10px;
+  }
+
+  .message-content {
+    flex: 1;
   }
 </style>
--- src/pages/left-Menu/Settings.svelte
+++ src/pages/left-Menu/Settings.svelte
@@ -1,141 +1,105 @@
 <script>
-  import { Page, Navbar, Block, BlockTitle,ListItem ,Icon,  List , Toggle ,f7  } from 'framework7-svelte';
-  
+  import { Page, Navbar, Block, BlockTitle, ListItem, Icon, List, Toggle, f7 } from 'framework7-svelte';
 
   let globalTheme = 'light';
-  let globalThemeColor = document.documentElement.style
-    .getPropertyValue('--f7-color-primary')
-    .trim();
-  let number ="95 123 456"; 
-  let  lang = "english"; 
+  let number = "95 123 456";
+  let lang = "english";
   let dark_mode = false;
-
 
   function setScheme(newTheme) {
     console.log(newTheme);
     f7.setDarkMode(newTheme === 'dark');
     globalTheme = newTheme;
-    theme = newTheme;
   }
 
-  function handleChange(e) {   
+  function handleChange(e) {
     lang = e.target.value;
   }
 
   function handleToggle(event) {
-    toggleValue = event.target.checked;
-
+    dark_mode = event.target.checked;
+    setScheme(dark_mode ? 'dark' : 'light');
   }
-
-  
 </script>
 
 <Page>
   <Navbar title="Settings" backLink="Back" />
   <div class="inline-container-header">
-    <Icon f7="gear_alt_fill" style="margin-top: 15px;"></Icon> 
+    <Icon f7="gear_alt_fill" style="margin-top: 15px;"></Icon>
     <BlockTitle>Settings</BlockTitle>
   </div>
   <Block>
-    <table width=50%>
+    <table style="margin: 0 auto;">
       <tr>
         <td>
           <a class="button button-round button-tonal" href>
-            <img class="icon" src='https://cdn-icons-png.flaticon.com/512/597/597177.png' alt="icon"> 
+            <img class="icon" src='https://cdn-icons-png.flaticon.com/512/597/597177.png' alt="icon">
             Number
-          </a> 
+          </a>
         </td>
         <td>
           <div class="input-container">
-            <input type="tel" class="phone-input" bind:value={number}  />
+            <input type="tel" class="phone-input" bind:value={number} />
           </div>
-          
-
-
-          
         </td>
       </tr>
 
       <tr>
         <td>
           <a class="button button-round button-tonal" href>
-            <img class="icon" src='https://cdn-icons-png.flaticon.com/512/2115/2115307.png' alt="icon"> 
+            <img class="icon" src='https://cdn-icons-png.flaticon.com/512/2115/2115307.png' alt="icon">
             Language
-            </a> 
+          </a>
         </td>
-      
+
         <td>
-        
           <List strongIos outlineIos dividersIos>
-            <ListItem title={lang}   smartSelect>
-              <select name="Languages" bind:value={lang} > <!--on:change={handleChange} -->
+            <ListItem title={lang} smartSelect>
+              <select name="Languages" bind:value={lang} style="text-align: center;">
+                <!--on:change={handleChange} -->
                 <option value="english">English</option>
                 <option value="french">French</option>
                 <option value="arabic">Arabic</option>
               </select>
             </ListItem>
           </List>
-
-
-
-      
         </td>
       </tr>
 
       <tr>
         <td>
           <a class="button button-round button-tonal" href>
-            <img class="icon" src='https://cdn-icons-png.flaticon.com/512/8338/8338708.png' alt="icon"> 
+            <img class="icon" src='https://cdn-icons-png.flaticon.com/512/8338/8338708.png' alt="icon">
             Dark Mode
-            </a> 
+          </a>
         </td>
-        
+
         <td>
-          <div style="margin-left: 10px;" >
-            <Toggle bind:checked={dark_mode} on:change={handleToggle} color="black"  />
+          <div style="margin-left: 10px;">
+            <Toggle bind:checked={dark_mode} on:change={handleToggle} color="black" />
           </div>
-      </td>
+        </td>
       </tr>
-
-
-
     </table>
   </Block>
-
-
-
-    <h1>{number}</h1>
-    <h1>{lang}</h1>
-    <h1>{dark_mode}</h1>
-
-  
 </Page>
 
-
-
-
 <style>
-   .icon{
+  .icon {
     width: 25px;
   }
-
 
   .input-container {
     display: flex;
     align-items: center;
   }
 
-
   .inline-container-header {
     display: flex;
     align-items: center;
     gap: 1px;
     margin-left: 10px;
-    
-    }
-
-
-
+  }
 
   .phone-input {
     padding: 8px;
@@ -143,5 +107,6 @@
     border-radius: 4px;
     width: 200px;
     outline: none;
+    text-align: center;
   }
 </style>
