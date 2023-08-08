72bf2da68bdfa4be18b734bd1ccd969ced0eb27e
Aslan <hacakirdogan@gmail.com>
2023-08-08 17:39:02 +0300

    update

--- src/pages/left-Menu/BusSchedule.svelte
+++ src/pages/left-Menu/BusSchedule.svelte
@@ -1,10 +1,36 @@
+<script>
+  import { onMount } from "svelte";
+  import L from "leaflet";
+  import { Block, Page, Navbar, Fab, Icon } from "framework7-svelte";
+  import WasalniNavBar from "../essentials/fixNavbar.svelte";
+
+  onMount(() => {
+    // Create a map instance
+    const map = L.map("map").setView([35.66, 10.79], 11);
+
+    // Add the OpenStreetMap tile layer to the map using Axios for HTTP requests
+    const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+    const attribution =
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    L.tileLayer(url, { attribution }).addTo(map);
+
+    L.marker([35.68421135126204, 10.841596169918398])
+      .addTo(map)
+      .on("click", () => alert("Clicked on ${station.name}"));
+  });
+</script>
+
 <Page>
-  <Navbar title="About" backLink="Back" />
-  <BlockTitle>About My App</BlockTitle>
+  <br />
+  <WasalniNavBar />
   <Block>
-    <p>busssssss</p>
+    <div id="map" />
   </Block>
 </Page>
-<script>
-  import { Page, Navbar, Block, BlockTitle } from 'framework7-svelte';
-</script>
+
+<style>
+  #map {
+    width: 100%;
+    height: 650px;
+  }
+</style>
